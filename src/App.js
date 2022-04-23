f669aa419ce55db9fcb1cdfef0e19e0843ddb9fa
Laura Santi <89958677+Lauraitnas@users.noreply.github.com>
2022-04-23 10:14:01 +0800

    projects added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
+//style
+import "./App.css";
+
 //components
 import Nav from "./components/Nav/Nav";
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,19 @@ import React from "react";
 //data
 import {allProjects} from "../data";
 
+//components
+import ProjectCard from "../components/ProjectCard/ProjectCard";
+
 function HomePage() {
     
     return (
-    <div>
-        {allProjects.map((projectData, key) => {
-            return <div key={key}>{projectData.title}</div>;
+    <div id="project-list">
+        {allProjects.map((projectData) => {
+            return (
+                <ProjectCard 
+                key={`project-${projectData.id}`} 
+            projectData={projectData} />
+            );
         })}
     </div>
     );
